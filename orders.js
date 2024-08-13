5f9414827306ecd9774e3490aa5aa206baf0a2d3
AkiraYumoto <juanicaehd@gmail.com>
2024-08-13 18:20:14 -0300

    Sidebar creada al español y sección de analiticas añadida con 3 porcentajes de ventas, visitas y busquedas

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    { 
+        productName: "Javascript Tutorial",
+        productNumber: '85874',
+        paymentStatus: 'Pendiente',
+        status: 'Pendiente'
+    },
+    {
+        productName: 'CSS Full Course',
+        productNumber: '94234',
+        paymentStatus: 'Pendiente',
+        status: "Rechazado"
+    },
+    {
+        productName: 'Flex-Box Tutorial',
+        productNumber: '35564',
+        paymentStatus: 'Aprobado',
+        status: "Activo"
+    }
+]
